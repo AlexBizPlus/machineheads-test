62e7fec5d9592ad607fc0740111628e445e978bd
AlexBizPlus <alexeynejnov@gmail.com>
2024-12-24 18:51:45 +0700

    add home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import { Login } from "./pages/login";
 import { PostList } from "./pages/post-list";
+import { Home } from "./pages/home/home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route index element={<Home />} />
         <Route path="login" element={<Login />} />
         <Route path="posts" element={<PostList />} />
       </Routes>
