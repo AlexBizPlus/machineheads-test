503dd7f97bc43d05d83c869e5188fafab7e3a0ee
AlexBizPlus <alexeynejnov@gmail.com>
2024-12-25 11:24:05 +0700

    update routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import { Login } from "./pages/login";
 import { PostList } from "./pages/post-list";
 import { Home } from "./pages/home/home";
+import { NoMatch } from "./pages/no-match/no-match";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="login" element={<Login />} />
-        <Route path="posts" element={<PostList />} />
-      </Routes>
-    </BrowserRouter>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/posts" component={PostList} />
+        <Route component={NoMatch} />
+      </Switch>
+    </Router>
   );
 }
 
